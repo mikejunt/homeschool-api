3c6484955cd06a3c1e4068686071cabe9e89751f
mikejunt <mikejunt@gmail.com>
2020-08-17 12:46:26 -0500

    configures secrets & connectionstring

--- Startup.cs
+++ Startup.cs
@@ -27,26 +27,29 @@ namespace homeschool_api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            services.AddDbContext<d90169mkp6oi7oContext>(options =>
+                options.UseNpgsql(Configuration["ConnectionString"]));
         }
     }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
 }
+
